804ea05b16ec2489365c512e92237d8acf957362
Deleon Simoni <deleon.simoni@gmail.com>
2021-10-14 18:12:52 -0300

    melhorias motor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { INgxSelectOption } from 'ngx-select-ex';
 import { EspecialidadeService } from '../services/especialidades.service';
 
 @Component({
@@ -19,7 +21,8 @@ export class HomeComponent implements OnInit {
 
 
   constructor(
-    private especialidadeSevice: EspecialidadeService) { }
+    private especialidadeSevice: EspecialidadeService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.listarEspecialidades();
@@ -33,18 +36,28 @@ export class HomeComponent implements OnInit {
     }
   }
 
+
+  normalizeNome(options: INgxSelectOption[]){
+    if(options){
+
+      options[0].text = options[0].data.nome;
+
+    }
+    
+  }
+
   consultar(pesquisa){
-    let filtro: any = this.especialidades.filter(e => e._id == pesquisa);
+
+    let filtro: any = this.especialidades.filter(e => e._id == pesquisa)[0];
 
     if(!filtro.type || filtro.type == 1){
-      //Especialidade
-
+      this.router.navigate([`/list/${filtro._id}`]);
     } else if(filtro.type == 2){
       //Veterinario
-
+      this.router.navigate([`/detail/${filtro.nome}/RioDeJaneiro`]);
     } else if(filtro.type == 3){
       //clinica
-
+      this.router.navigate([`/detail/${filtro.nome}/RioDeJaneiro`]);
     }
   }
 
@@ -54,12 +67,33 @@ export class HomeComponent implements OnInit {
     this.especialidadeSevice.getCompletedFind(query)
       .subscribe(
         data => {
-          data.forEach(item => item.nomeFormated = item['nome'] + ' ' + item['nomeFormated']);
+          data.forEach(item => item.nomeFormated = item['nome'] + ' ' + item['nomeFormated'] + ' ' + this.createEdgeNGrams(item['nomeFormated']));
           this.especialidades = data;
         },
         error => {
           console.log(error);
         });
+  }
+
+  
+  createEdgeNGrams(str) {
+    if (str && str.length > 3) {
+        const minGram = 3
+        const maxGram = str.length
+        
+        return str.split(" ").reduce((ngrams, token) => {
+            if (token.length > minGram) {   
+                for (let i = minGram; i <= maxGram && i <= token.length; ++i) {
+                    ngrams = [...ngrams, token.substr(0, i)]
+                }
+            } else {
+                ngrams = [...ngrams, token]
+            }
+            return ngrams
+        }, []).join(" ")
+    } 
+    
+    return str
   }
 
   listarEspecialidades() {
