3d2a50f203cf1a3c624d6898fbf1d48262261e80
unknown <regilima@gmail>
2021-11-27 00:59:09 -0300

    foto no perfil, add link inscreva se

--- src/app/form-contato/form-contato.component.ts
+++ src/app/form-contato/form-contato.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FormContatoComponent implements OnInit {
 
   base = 'Form';
-  page = 'Contato';
+  page = 'Cadastre-se';
 
   constructor() { }
 
