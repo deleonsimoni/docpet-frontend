dd9bb45df7ff5231391d9ff6e0931fb9e9bb7145
Regina weigert <reginaweigert@gmail.com>
2022-01-04 10:39:12 -0300

    Inclusao de rotas em seo-sitemap

--- src/app/seo-sitemap.ts
+++ src/app/seo-sitemap.ts
@@ -10,6 +10,18 @@ export const seoSitemap: ISitemapTag[] = [
       title: 'Alergista Veterinário: Encontre aqui! | VetzCo', 
       description: 'Some description about', 
       image: 'veterinario alergista sp 40, alergia veterinaria 40, alergista veterinário 30, alergia em pets 10, alergista veterinario 10' 
+    },
+    { 
+      customUrl: '/cadastro', 
+      title: 'Cadastre-se para intregar a plataforma de veterinários e clínicas | VetzCo', 
+      description: 'Nossa missão é ajudar médicos veterinários e locais de pet a criarem identidade digital', 
+      image: 'plataforma de veterinários, clínicas veterinárias, pets, petshop' 
+    },
+    { 
+      customUrl: '/login', 
+      title: 'Faça login para acessar sua conta na plataforma de veterinários, clínicas e locais de pet | VetzCo', 
+      description: 'Acesso restrito a plataforma de veterinários, clínicas e locais de pet', 
+      image: 'login para a plataforma Vetzco' 
     }
   ];
   
