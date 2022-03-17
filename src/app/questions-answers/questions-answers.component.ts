e544650e407c5c99086510a7ba9d33959ad0ef0d
Regina weigert <reginaweigert@gmail.com>
2022-03-17 14:06:25 -0300

    Ajuste em perguntas e respostas

--- src/app/questions-answers/questions-answers.component.ts
+++ src/app/questions-answers/questions-answers.component.ts
@@ -7,11 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsAnswersComponent implements OnInit {
 vermais: boolean = false;
+maisResposta: boolean = false;
+public showMaisRespostas = 3;
+public showTotalRespostas = 0;  
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
-  getMais(){
-    this.vermais = true;
+  getMais(num){
+   // this.vermais = true;
+   var formElement = <HTMLFormElement>document.getElementById('mais'+num);
+   formElement.style.display='block';
+    this.maisResposta = false;
+  }
+  
+  getMinus(idp){
+    var formElement = <HTMLFormElement>document.getElementById('mais'+idp);
+    formElement.style.display='none';
+    var scrollPos = $("#card"+idp).offset().top;
+   $("body,html").animate({scrollTop: scrollPos}, "slow");
+    
+  }
+  getMore(respostas){
+    
   }
 }
